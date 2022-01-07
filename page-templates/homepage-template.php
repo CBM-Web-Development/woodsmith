31d89b83598124740e8578da8cbaf555d2462683
Connor Meehan <cmeehan@elon.edu>
2022-01-07 14:46:40 -0500

    Updating page and hompage

--- page-templates/homepage-template.php
+++ page-templates/homepage-template.php
@@ -2,21 +2,22 @@
 /* Template Name: Homepage Template */
 get_header(); 
 ?>
-
-<div class='row gx-0'>
-	
-	<?php if(have_posts()){
-		while(have_posts()){ 
-			the_post(); ?>
-			
-			<div class="col-md-12">
-				<?php the_content(); ?>
-			</div>
-		<?php }// end while
-	} else{ ?>
+<div class="container-fluid g-0">
+	<div class='row g-0'>
 		
-	<?php }	?>
-	
+		<?php if(have_posts()){
+			while(have_posts()){ 
+				the_post(); ?>
+				
+				<div class="col-md-12">
+					<?php the_content(); ?>
+				</div>
+			<?php 
+			}// end while
+		} //end if 
+		?>
+		
+	</div>
 </div>
 
 <?php get_footer();
--- page.php
+++ page.php
@@ -1,27 +1,40 @@
 <?php get_header();?>
 <div class="container-fluid">
-	<div class="row">
-		<?php 
-		if(get_the_post_thumbnail()){
-			$thumbnail_url = get_the_post_thumbnail_url(); 
-		}else{
-			$thumbnail_url = get_header_image();
-		}
-		?>
-		<div class="page-header-container"  style="background-image:url(<?php echo $thumbnail_url; ?>);">
-			<div class="page-header-content h-100 d-flex justify-content-center">
-				<h1 class="page-header-title align-self-center"><?php the_title(); ?></h1>
+	<?php if(have_posts()){
+		while(have_posts()){
+			the_post(); ?>
+			
+			<div class="row">
+				<?php 
+				$header_class = 'page-header-container';
+				if(has_post_thumbnail()){
+					$thumbnail_url = get_the_post_thumbnail_url(); 
+					$header_class .=  '--has-image';
+				}else{
+					$header_class .='--no-image';
+				}
+				?>
+				<div class="<?php echo $header_class?>"  <?php if(has_post_thumbnail()){ ?> style="background-image:url(<?php echo $thumbnail_url; ?>);" <?php } ?>">
+				<?php if(!has_post_thumbnail() && function_exists('yoast_breadcrumb')){
+					echo '<div class=" col-md-12 col-lg-10 mx-lg-auto">';
+					yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+					echo '</div>';
+				}?>					<?php if(has_post_thumbnail()){?>
+				<div class="mask"></div>
+				<?php } ?>
+					<h1 class="page-header-title<?php if(!has_post_thumbnail()){ echo " col-md-12 col-lg-10 mx-lg-auto"; }else{ echo "--has-image";}?>"><?php the_title(); ?></h1>
+					
+
+				</div>
 			</div>
-		</div>
-	</div>
-	<div class="row">
-		<?php if(have_posts()): while(have_posts()):the_post(); ?>
-		<div class="col-md-10 mx-auto">
-			<?php the_content();?>		
-		</div>
-		<?php endwhile; else: ?>
-		
-		<?php endif; ?>
-	</div>	
+			<div class="row">
+				<div class="col-md-10 mx-auto">
+					<?php the_content(); ?>
+				</div>
+			</div>
+			
+			<?php
+		}
+	} ?>
 </div>
 <?php get_footer();?>
