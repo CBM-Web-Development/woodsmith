d683199d37a796896c3a9fe12be3429ac45f6535
Connor Meehan <cmeehan@elon.edu>
2022-09-09 20:04:00 +0000

    Update head tag

--- partials/head-tags.php
+++ partials/head-tags.php
@@ -1,11 +1,10 @@
-<!-- Google Analytics -->
+element. Don’t add more than one Google tag to each page.
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SV4L0HQWDL"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-74857924-4', 'auto');
-  ga('send', 'pageview');
-
+  gtag('config', 'G-SV4L0HQWDL');
 </script>
