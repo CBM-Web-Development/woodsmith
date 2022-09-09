c6060a90cd076264c63beb8780b6b7315937771b
Connor Meehan <cmeehan@elon.edu>
2022-09-09 17:07:22 +0000

    Adding facebook verification

--- partials/head-meta.php
+++ partials/head-meta.php
@@ -2,3 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="The Woodsmith of Hilton Head Island is a local carpenter serving the Hilton Head Island and surrounding communities. The Woodsmith specializes in custom plantation shutters and other wood craft such as mantles, jewelry boxes, humidors, and other specialized and custom items.">
 <meta name="keywords" content="woodsmith, wood, woodworker, carpenter, plantation shutter, plantation shutters, window, windows, window treatment, jewelry, craftsman, hilton head island, hhi, hilton head, savannah, low country, beaufort, sc, south carolina, charleston, georgia">
+
+<meta name="facebook-domain-verification" content="s0arp4esb0led2tsa74w3ec4ua6079" />
